01a812d8cf4bc12f0ef06e3e7c959ce6e3f3fa35
b-small <sblagovesta@yahoo.com>
2014-10-20 01:04:41 +0300

    encapsulation up

--- 2DShooter/_2DShooter/Game1.cs
+++ 2DShooter/_2DShooter/Game1.cs
@@ -12,18 +12,8 @@ using Microsoft.Xna.Framework.Media;
 
 namespace _2DShooter
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class Game1 : Microsoft.Xna.Framework.Game
     {
-        public enum State
-        {
-            Menu,
-            Playing,
-            Gameover
-        }
-
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Random random = new Random();
@@ -72,8 +62,15 @@ namespace _2DShooter
             sf.LoadContent(Content);
             hud.LoadContent(Content);
             sm.LoadContent(Content);
-            menuImage = Content.Load<Texture2D>("menuImage");
-            gameoverImage = Content.Load<Texture2D>("skull");
+            try
+            {
+                menuImage = Content.Load<Texture2D>("menuImage");
+                //gameoverImage = Content.Load<Texture2D>("skull");
+            }
+            catch (ImageNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         protected override void UnloadContent()
@@ -152,7 +149,14 @@ namespace _2DShooter
 
             if (asteroidList.Count < 5)
             {
-                asteroidList.Add(new Asteroid(Content.Load<Texture2D>("asteroid"), new Vector2(randX, randY)));
+                try
+                {
+                    asteroidList.Add(new Asteroid(Content.Load<Texture2D>("asteroid"), new Vector2(randX, randY)));
+                }
+                catch (ImageNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
 
             for (int i = 0; i < asteroidList.Count; i++)
@@ -172,12 +176,19 @@ namespace _2DShooter
 
             if (enemyList.Count() < 3)
             {
-                enemyList.Add(new Enemy(Content.Load<Texture2D>("enemyship"), new Vector2(randX, randY), Content.Load<Texture2D>("EnemyBullet")));
+                try
+                {
+                    enemyList.Add(new Enemy(Content.Load<Texture2D>("enemyship"), new Vector2(randX, randY), Content.Load<Texture2D>("EnemyBullet")));
+                }
+                catch (ImageNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
 
             for (int i = 0; i < enemyList.Count; i++)
             {
-                if (!enemyList[i].isVisible)
+                if (!enemyList[i].IsVisible)
                 {
                     enemyList.RemoveAt(i);
                     i--;
@@ -201,34 +212,41 @@ namespace _2DShooter
         protected void UpdatePlaying(GameTime gameTime)
         {
             sf.speed = 5;
-           
+
             foreach (Enemy e in enemyList)
             {
-                if (e.boundingBox.Intersects(p.boundingBox))
+                if (e.BoundingBox.Intersects(p.BoundingBox))
                 {
-                    p.health -= 40;
-                    e.isVisible = false;
+                    p.Health -= 40;
+                    e.IsVisible = false;
                 }
 
                 //bullet collision
-                for (int i = 0; i < e.bulletList.Count; i++)
+                for (int i = 0; i < e.BulletList.Count; i++)
                 {
-                    if (p.boundingBox.Intersects(e.bulletList[i].boundingBox))
+                    if (p.BoundingBox.Intersects(e.BulletList[i].boundingBox))
                     {
-                        p.health -= enemyBulletDamage;
-                        e.bulletList[i].isVisible = false;
+                        p.Health -= enemyBulletDamage;
+                        e.BulletList[i].isVisible = false;
                     }
                 }
 
-                for (int i = 0; i < p.bulletList.Count; i++)
+                for (int i = 0; i < p.BulletList.Count; i++)
                 {
-                    if (p.bulletList[i].boundingBox.Intersects(e.boundingBox))
+                    if (p.BulletList[i].boundingBox.Intersects(e.BoundingBox))
                     {
                         sm.explodeSound.Play();
-                        explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion3"), new Vector2(e.position.X, e.position.Y)));
+                        try
+                        {
+                            explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion3"), new Vector2(e.position.X, e.position.Y)));
+                        }
+                        catch (ImageNotFoundException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
                         hud.playerScore += 20;
-                        p.bulletList[i].isVisible = false;
-                        e.isVisible = false;
+                        p.BulletList[i].isVisible = false;
+                        e.IsVisible = false;
                     }
                 }
 
@@ -243,32 +261,39 @@ namespace _2DShooter
             //update&check asteroids for collision
             foreach (Asteroid a in asteroidList)
             {
-                if (a.boundingBox.Intersects(p.boundingBox))
+                if (a.boundingBox.Intersects(p.BoundingBox))
                 {
-                    p.health -= 20;
+                    p.Health -= 20;
                     a.isVisible = false;
                 }
 
                 //iterate through the bulletList and check for collision
-                for (int i = 0; i < p.bulletList.Count; i++)
+                for (int i = 0; i < p.BulletList.Count; i++)
                 {
-                    if (a.boundingBox.Intersects(p.bulletList[i].boundingBox))
+                    if (a.boundingBox.Intersects(p.BulletList[i].boundingBox))
                     {
                         sm.explodeSound.Play();
-                        explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion3"), new Vector2(a.position.X, a.position.Y)));
+                        try
+                        {
+                            explosionList.Add(new Explosion(Content.Load<Texture2D>("explosion3"), new Vector2(a.position.X, a.position.Y)));
+                        }
+                        catch (ImageNotFoundException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
                         hud.playerScore += 5;
                         a.isVisible = false;
-                        p.bulletList.ElementAt(i).isVisible = false;
+                        p.BulletList.ElementAt(i).isVisible = false;
                     }
                 }
 
                 a.Update(gameTime);
             }
 
-            if (p.health <= 0)
+            if (p.Health <= 0)
             {
                 gameState = State.Gameover;
-            } 
+            }
 
             hud.Update(gameTime);
             p.Update(gameTime);
@@ -301,7 +326,7 @@ namespace _2DShooter
                 p.position = new Vector2(400, 900);
                 enemyList.Clear();
                 asteroidList.Clear();
-                p.health = 200;
+                p.Health = 200;
                 hud.playerScore = 0;
                 gameState = State.Menu;
             }
